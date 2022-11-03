3dcbf9822e38fc3e777f0fd38b2f7c2668f0beaf
wilypradana <pradanawily@gmail.com>
2022-11-03 16:13:33 +0700

    updated

--- baju-adat.cpp
+++ baju-adat.cpp
@@ -53,7 +53,81 @@ int main()
             kbln = jmlh - tot;
             cout << "kembalian kamu: " << kbln;
         }
+
+    case 2:
+        cout << "masukan ukuran pakaian adat(20,35,45,50): ";
+        cin >> up;
+        switch (up)
+        {
+        case 50:
+            hsp = 11000;
+            break;
+        case 45:
+            hsp = 9000;
+            break;
+        case 35:
+            hsp = 7000;
+            break;
+        case 20:
+            hsp = 5000;
+            break;
+        }
+        cout << "masukan lama sewa pakaian adat: ";
+        cin >> ls;
+        cout << "NAMA PAKAIAN ADAT:PAKAIAN ADAT JAWA TENGAH " << endl;
+        cout << "UKURAN PAKAIAN ADAT: " << up << endl;
+        cout << "HARGA SEWA PERHARI: " << hsp << endl;
+        tot = ls * hsp;
+        cout << "JUMLAH SEWA YANG HARUS DIBAYAR: " << tot << endl;
+        cout << "BAYAR SEKARANG: ";
+        cin >> jmlh;
+        if (jmlh < tot)
+        {
+            cout << "ups,jumlahnya kurang nih";
+        }
+        else
+        {
+            kbln = jmlh - tot;
+            cout << "kembalian kamu: " << kbln;
+        }
+    case 3:
+        cout << "masukan ukuran pakaian adat(20,35,45,50): ";
+        cin >> up;
+        switch (up)
+        {
+        case 50:
+            hsp = 11000;
+            break;
+        case 45:
+            hsp = 9000;
+            break;
+        case 35:
+            hsp = 7000;
+            break;
+        case 20:
+            hsp = 5000;
+            break;
+        }
+        cout << "masukan lama sewa pakaian adat: ";
+        cin >> ls;
+        cout << "NAMA PAKAIAN ADAT:PAKAIAN ADAT JAWA TIMUR " << endl;
+        cout << "UKURAN PAKAIAN ADAT: " << up << endl;
+        cout << "HARGA SEWA PERHARI: " << hsp << endl;
+        tot = ls * hsp;
+        cout << "JUMLAH SEWA YANG HARUS DIBAYAR: " << tot << endl;
+        cout << "BAYAR SEKARANG: ";
+        cin >> jmlh;
+        if (jmlh < tot)
+        {
+            cout << "ups,jumlahnya kurang nih";
+        }
+        else
+        {
+            kbln = jmlh - tot;
+            cout << "kembalian kamu: " << kbln;
+        }
     }
 
     return 0;
 }
+
